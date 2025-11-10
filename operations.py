302ff5beddb033c284a0226868a87fe713685a7b
Shrusti.koudi <01fe24ica019@kletech.ac.in>
2025-11-10 11:35:03 +0530

    Addition and subtractions of num

---
+++ operations.py
@@ -0,0 +1,12 @@
+#taking input from the user
+num1= float(input("Enter first number:"))
+num2= float(input("Enter second number:"))
+
+#performing addition and subtraction
+sum_result=num1+num2
+diff_result=num1-num2
+
+#displaying the results
+print("\nResults:")
+print(f"Addition:{num1}+{num2}={sum_result}")
+print(f"Substraction:{num1}-{num2}={diff_result}")
